7bbe55c025eea5483652684d4dee7fbd5aef67ac
chaprevetoon <90418336+chaprevetoon@users.noreply.github.com>
2025-08-06 19:52:01 -0300

    Add files via upload

--- Aula 9/Atividade2.py
+++ Aula 9/Atividade2.py
@@ -15,9 +15,16 @@ for contador in range(3):
     login = input('Login:  ')
     senha_login = input('Senha de login: ')
     while login in dado['login'] and senha in dado['senha']:
+        aluno = input(f'''Qual aluno será avaliado?
+                      Exemplos:
+                      Aluno1 -> 1
+                      Aluno2 -> 2
+                      ...
+                      AlunoN -> n''')
         n1 = float(input("Digite a 1 nota: "))
         n2 = float(input("Digite a 2 nota: "))
-        media = float((n1 + n2) / 2)
+        n3 = float(input("Digite a 3 nota: "))
+        media = float((n1 + n2 + n3) / 3)
         print("A média do aluno é :", media)
         if media < 7:
             print(f"{login} foi Reprovado")
@@ -26,6 +33,5 @@ for contador in range(3):
         break
     else:
         print("Login ou senha incorreta")
-    break
 else:
     print("conta bloqueada")
